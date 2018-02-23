c713e3027ec7588bf61f90a2d6747979c8052b50
IHEB-PC\IHEB <ihebelfekih@gmail.com>
2018-02-23 02:10:24 +0100

    tp1 modif commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Observable} from "rxjs/Observable";
-import {AngularFireDatabase} from "angularfire2/database";
+import {AngularFireDatabase, AngularFireList} from "angularfire2/database";
 
 @Component({
   selector: 'app-root',
@@ -9,11 +9,43 @@ import {AngularFireDatabase} from "angularfire2/database";
 })
 
 export class AppComponent implements OnInit {
-  description: Observable<any>;
+  
+ 
+  email: string;
+  objet: string;
+  text: string;
+  itemsRef: AngularFireList<any>;
+  items: Observable<any[]>;
 
-  constructor(private db: AngularFireDatabase)
+  message = {
+    email:'',
+    text:'',
+    objet:''
+  }
+
+  constructor(db: AngularFireDatabase) {
+    this.itemsRef = db.list('messages');
+    // Use snapshotChanges().map() to store the key
+    this.items = this.itemsRef.snapshotChanges().map(changes => {
+      return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+    });
+  }
+  
+  addMessage() {
+    this.itemsRef.push({ text: this.text , email:this.email,objet:this.objet});
+  }
+  updateMessage(key: string, newText: string , newEmail : string, newObject:string) {
+    this.itemsRef.update(key, { text: newText , email:newEmail,objet:newObject }).then(_ => console.log('success'))
+    .catch(err => console.log(err, 'You dont have access!'));
+  }
+  deleteMessage(key: string) {    
+    this.itemsRef.remove(key); 
+  }
+   getMessage(key: string, newText: string , newEmail : string, newObject:string)
   {
-  	this.description=db.object('description').valueChanges();
+this.message.email= newText;
+this.message.text = newEmail;
+this.message.objet=newObject;
   }
 
   ngOnInit()
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {AngularFireModule} from "angularfire2";
 import {AngularFireDatabaseModule} from "angularfire2/database";
+import { FormsModule }   from '@angular/forms';
 
-const config = {
+ const config = {
     apiKey: "AIzaSyBWsj7a-6seEKnaZME_38fVnNnceFfLli8",
     authDomain: "tp-angularfire-afcf0.firebaseapp.com",
-    databaseURL: "https://tp-angularfire-afcf0.firebaseio.com",
+    databaseURL: "https://tp-angularfire-afcf0.firebaseio.com/",
     projectId: "tp-angularfire-afcf0",
     storageBucket: "tp-angularfire-afcf0.appspot.com",
     messagingSenderId: "130638706521"
@@ -19,7 +20,9 @@ const config = {
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(config),
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+     FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
